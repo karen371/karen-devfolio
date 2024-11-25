71407711c098eb6d8473802c565466855aee2dd6
karen371 <karengodoy325@gmail.com>
2024-11-25 20:19:32 -0300

    primer commit

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: 'https://karen371.github.io/Prueba-Desarrollo-de-interfaces/karen-devfolio'
+  base: 'https://karen371.github.io/karen-devfolio/'
 })
