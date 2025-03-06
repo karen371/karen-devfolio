774e4eaa38607f6c1f29a26e9d49f38d60df3e86
karen371 <karengodoy325@gmail.com>
2025-03-06 19:22:41 -0300

    agregar nueva certificacion

--- src/store/certificaciones.js
+++ src/store/certificaciones.js
@@ -3,6 +3,7 @@ import cDesafio from '../assets/img/images.jpg';
 import cInovacien from '../assets/img/cropped-favicon.png';
 import TSanto from '../assets/img/logoSantoTomas.png';
 import cStanford from '../assets/img/logostanford.png';
+import tdigital from '../assets/img/talento-digital.png';
 const state = () => ({
     certificados: [
     /** Lenguajes de marcado */
@@ -11,6 +12,8 @@ const state = () => ({
     { imageSrc: cDesafio, link:'https://cursos.desafiolatam.com/certificates/iotsgfuybi', nombre: 'CSS Avanzado' },
     { imageSrc: cStanford, link:'https://codeinplace.stanford.edu/cip3/certificate/eds9e6', nombre: 'Python' },
     { imageSrc: cInovacien, link:'', nombre: 'Big data' },
+    { imageSrc: tdigital, link:'https://drive.google.com/file/d/185gWiSktMu7t1pfHp6OI72IXzvjXKM3X/view?usp=sharing', nombre:'DESARROLLO APLICACIONES FRONT- END TRAINEE'}
+
   ]
 });
 
